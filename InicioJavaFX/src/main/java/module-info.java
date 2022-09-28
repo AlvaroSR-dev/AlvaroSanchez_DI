aa970e9a414334cb0c85596152a9e1f102348b7f
Álvaro Sánchez <73520829+AlvaroSR-dev@users.noreply.github.com>
2022-09-28 12:24:24 +0200

    update

---
+++ InicioJavaFX/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.iniciojavafx {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.iniciojavafx to javafx.fxml;
+    exports com.example.iniciojavafx;
+}

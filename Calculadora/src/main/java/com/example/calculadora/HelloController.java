116dc610647ba620116b82c0edd6051396fe5376
Álvaro Sánchez <73520829+AlvaroSR-dev@users.noreply.github.com>
2022-10-17 14:27:43 +0200

    update

--- Calculadora/src/main/java/com/example/calculadora/HelloController.java
+++ Calculadora/src/main/java/com/example/calculadora/HelloController.java
@@ -1,11 +1,17 @@
 package com.example.calculadora;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 
-public class HelloController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HelloController implements Initializable {
     @FXML
     private TextArea resultado;
 
@@ -13,4 +19,13 @@ public class HelloController {
     private Button sci, registro, uno, dos, tres, cuatro, cinco, seis, siete, ocho, nueve, cero, ac, positivoNegativo, modulo, dividir, multiplicar, restar, sumar, igual, numeroE, decimal;
 
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+            uno.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent actionEvent) {
+                    resultado.setText(resultado.getText()+uno.getText());
+                }
+            });
+    }
 }

d2f365364797b4605a0b28fde7538f491af9fc0a
Álvaro Sánchez <73520829+AlvaroSR-dev@users.noreply.github.com>
2022-10-20 12:47:22 +0200

    update

--- Calculadora/src/main/java/com/example/calculadora/HelloController.java
+++ Calculadora/src/main/java/com/example/calculadora/HelloController.java
@@ -4,21 +4,27 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
-import javax.script.*;
+import javafx.scene.layout.GridPane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class HelloController implements Initializable {
-    int numResultado = 0;
-    ScriptEngineManager scriptEngineManager = new ScriptEngineManager();
-    ScriptEngine scriptEngine = scriptEngineManager.getEngineByName("Nashorn");
+    private double operando1 = 0.0;
+    private double operando2 = 0.0;
+    private double numResultado = 0.0;
+
+    String operacion = "";
 
     @FXML
     private TextArea resultado;
+
+    @FXML
+    private GridPane gridCalculadora;
 
     @FXML
     private Button sci, registro, uno, dos, tres, cuatro, cinco, seis, siete, ocho, nueve, cero, ac, positivoNegativo, modulo, dividir, multiplicar, restar, sumar, igual, numeroE, decimal;
@@ -97,33 +103,61 @@ public class HelloController implements Initializable {
                 }
             });
 
+            ac.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent actionEvent) {
+                    resultado.setText("");
+                }
+            });
+
             sumar.setOnAction(new EventHandler<ActionEvent>() {
                 @Override
                 public void handle(ActionEvent actionEvent) {
-                    resultado.setText(resultado.getText()+sumar.getText());
+                    operando1 = Double.parseDouble(resultado.getText());
+                    resultado.setText("");
+                    operacion = "suma";
                 }
             });
 
             restar.setOnAction(new EventHandler<ActionEvent>() {
                 @Override
                 public void handle(ActionEvent actionEvent) {
-                    resultado.setText(resultado.getText()+restar.getText());
+                    operando1 = Double.parseDouble(resultado.getText());
+                    resultado.setText("");
+                    operacion = "resta";
                 }
             });
-
-
 
             igual.setOnAction(new EventHandler<ActionEvent>() {
                 @Override
                 public void handle(ActionEvent actionEvent) {
-                    try {
-                        numResultado = (int) scriptEngine.eval(resultado.getText());
-                    } catch (ScriptException e) {
-                        e.printStackTrace();
+                    operando2 = Double.parseDouble(resultado.getText());
+                    switch (operacion){
+                        case "suma":
+                            numResultado = operando1+operando2;
+                            resultado.setText(String.valueOf(numResultado));
+                            break;
+                        case "resta":
+                            numResultado = operando1-operando2;
+                            resultado.setText(String.valueOf(numResultado));
+                            break;
                     }
-
-                    System.out.println(numResultado);
                 }
             });
+
+        /*for (Node item:gridCalculadora.getChildren()) {
+            if (item instanceof Button){
+                ((Button) item).setOnAction(new manejoPulsaciones());
+            }
+        }*/
+
+
     }
+    /*class manejoPulsaciones implements EventHandler<ActionEvent>{
+
+        @Override
+        public void handle(ActionEvent actionEvent) {
+
+        }
+    }*/
 }
